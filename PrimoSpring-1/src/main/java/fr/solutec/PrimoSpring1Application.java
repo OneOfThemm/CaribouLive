c675fb549dad80a1a1bddafa92feba154fb59a68
esic <esic@DESKTOP-HCETNMI>
2019-08-23 13:10:15 +0200

    First Commit

---
+++ PrimoSpring-1/src/main/java/fr/solutec/PrimoSpring1Application.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoSpring1Application.class, args);
+	}
+
+}
