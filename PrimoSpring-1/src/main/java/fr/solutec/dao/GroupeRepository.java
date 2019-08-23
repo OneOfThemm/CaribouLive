77b98a1289cb9535844eaed41830668e932593c1
CoralineReineOurs <coraline.leberre@gmail.com>
2019-08-23 14:44:33 +0200

    GroupRest v2

--- PrimoSpring-1/src/main/java/fr/solutec/dao/GroupeRepository.java
+++ PrimoSpring-1/src/main/java/fr/solutec/dao/GroupeRepository.java
@@ -1,10 +1,12 @@
 package fr.solutec.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.solutec.entities.Groupe;
 
 public interface GroupeRepository extends JpaRepository<Groupe, Long> {
 
 	public Groupe findByNom (String nom);
+	public Groupe deleteById (@PathVariable Long id);
 }
