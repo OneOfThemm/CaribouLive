6d645e0bbcd5ed772b249885d4a51c2f586829cc
OneOfThemm <tanguy.chanzy@consultants-solutec.fr>
2019-08-23 15:52:03 +0200

    sdfsfqd

--- PrimoSpring-1/src/main/java/fr/solutec/service/UserRest.java
+++ PrimoSpring-1/src/main/java/fr/solutec/service/UserRest.java
@@ -1,14 +1,9 @@
 package fr.solutec.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.solutec.dao.UserRepository;
-import fr.solutec.entities.User;
 
 
 
@@ -16,8 +11,7 @@ import fr.solutec.entities.User;
 @RestController
 @CrossOrigin("*")
 public class UserRest {
-	@Autowired
-	private UserRepository userRepos;
+
 	
 
 }
