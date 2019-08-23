5334d44f0dcde562e2132f66bc0b3f009423df07
esic <esic@DESKTOP-HCETNMI>
2019-08-23 14:06:24 +0200

    3rd

--- PrimoSpring-1/src/main/java/fr/solutec/service/UserRest.java
+++ PrimoSpring-1/src/main/java/fr/solutec/service/UserRest.java
@@ -1,5 +1,17 @@
 package fr.solutec.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.dao.UserRepository;
+
+
+
+@RestController
+@CrossOrigin("*")
 public class UserRest {
+	@Autowired
+	private UserRepository userRepos;
 
 }
