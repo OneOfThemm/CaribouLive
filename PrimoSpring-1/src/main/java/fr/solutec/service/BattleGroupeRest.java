5138c00ece354db8ee8c82087e091e3528ee66d5
OneOfThemm <tanguy.chanzy@consultants-solutec.fr>
2019-08-26 17:11:18 +0200

    fsqdfsdq

--- PrimoSpring-1/src/main/java/fr/solutec/service/BattleGroupeRest.java
+++ PrimoSpring-1/src/main/java/fr/solutec/service/BattleGroupeRest.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.dao.BattleGroupeRepository;
 import fr.solutec.entities.BattleGroupe;
-import fr.solutec.entities.Event;
+
 
 @RestController
 @CrossOrigin("*")
