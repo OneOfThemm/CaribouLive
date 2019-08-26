067ca54203cf4a5f84f218d4405ff9bc70ee65eb
Hackaribou <arthurv59@gmail.com>
2019-08-26 12:27:42 +0200

    fonction authentification non fonctionelle mais ne créé pas d'erreurs

--- PrimoSpring-1/src/main/java/fr/solutec/service/AuthentificationRest.java
+++ PrimoSpring-1/src/main/java/fr/solutec/service/AuthentificationRest.java
@@ -1,9 +1,11 @@
 package fr.solutec.service;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.solutec.dao.BarRepository;
 import fr.solutec.dao.ClientRepository;
 import fr.solutec.dao.GroupeRepository;
+
+import fr.solutec.entities.Bar;
 import fr.solutec.entities.Client;
+import fr.solutec.entities.Groupe;
 
 @RestController
 @CrossOrigin("*")
@@ -19,26 +24,47 @@ import fr.solutec.entities.Client;
 public class AuthentificationRest {
 	@Autowired
 	private ClientRepository clientRepo;
-	private BarRepository barRepo;
-	private GroupeRepository groupeRepo;
+	private BarRepository barRepoS;
+	private GroupeRepository groupeRepoS;
 
-	@RequestMapping(value = "/connexion/type", method = RequestMethod.GET)
-	public String getTypeUser(String mail) {
+	@RequestMapping(value = "/connexion/type/{mail:.+}", method = RequestMethod.GET)
+	public String getTypeUser(@PathVariable String mail) {
 		String type = "";
+		Client c = new Client();
+		c = null;
+		Bar b = new Bar();
+		b = null;
+		Groupe g = new Groupe();
+		g = null;
+		try {			
+			b = barRepoS.findByMail(mail);	
+			c = clientRepo.findByMail(mail);
+					
+			g = groupeRepoS.findByMail(mail);
 
-		if (clientRepo.findByMail(mail) != null) {
-			type = "client";
+		} catch (Exception e) {
+			System.err.println("erreur : " + e);
+		}
+		if (b != null) {
+			type = "bar";
 		} else {
-			if (clientRepo.findByMail(mail) != null) {
+			if (c != null) {
+				type = "client";
 
 			} else {
-				if (clientRepo.findByMail(mail) != null) {
-
+				if (g != null) {
+					type = "groupe";
+				} else {
+					type = "unknown";
 				}
 			}
 		}
-
+		/*
+		 * if (groupeRepo.findByMail(mail) != null) { type = "groupe"; } else { if
+		 * (barRepo.findByMail(mail) != null) { type = "bar"; } else { if
+		 * (clientRepo.findByMail(mail) != null) { type = "client"; } else { type =
+		 * "unknown"; } } }
+		 */
 		return type;
-
 	}
 }
