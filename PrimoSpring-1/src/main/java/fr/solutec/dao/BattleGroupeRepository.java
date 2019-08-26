67196d9df5d6384e655146fbc13a2e2b6c468d24
CoralineReineOurs <coraline.leberre@gmail.com>
2019-08-26 15:04:42 +0200

    BattleGroupeRest --> Avoir la liste des events futurs et passé en fonction d'un bar

--- PrimoSpring-1/src/main/java/fr/solutec/dao/BattleGroupeRepository.java
+++ PrimoSpring-1/src/main/java/fr/solutec/dao/BattleGroupeRepository.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.solutec.entities.BattleGroupe;
 import fr.solutec.entities.Event;
@@ -12,6 +13,8 @@ public interface BattleGroupeRepository extends JpaRepository<BattleGroupe, Long
 	public List<BattleGroupe> findByNom (String nom);
 	
 	public List<BattleGroupe> findByDateEventBefore (Date date); 
-	public List<BattleGroupe> findByDateEventAfter (Date date); 
-
+	public List<BattleGroupe> findByDateEventAfter (Date date);
+	
+	public List<BattleGroupe> getBattleGroupeByBarId (@PathVariable Long id);
+	public List<BattleGroupe> getBattleGroupeByGenreNom (@PathVariable String nom);
 }
--- PrimoSpring-1/src/main/java/fr/solutec/service/BattleGroupeRest.java
+++ PrimoSpring-1/src/main/java/fr/solutec/service/BattleGroupeRest.java
@@ -1,9 +1,12 @@
 package fr.solutec.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,5 +31,42 @@ public class BattleGroupeRest {
 	public List<BattleGroupe> getBattleGroupe(){
 		return battleGroupRepos.findAll();
 	}
+	
+	
+	@RequestMapping(value = "/battlegroupes/{id}", method = RequestMethod.GET)
+	public List<BattleGroupe> MyEvents (@PathVariable Long id){
+		return battleGroupRepos.getBattleGroupeByBarId(id);
+	}
+	
+	@RequestMapping(value = "/battlegroupes/futur/{id}", method = RequestMethod.GET)
+	public List<BattleGroupe> MyFuturEvents (@PathVariable Long id){
+		Date d = new Date();
+		List<BattleGroupe> myEvents = battleGroupRepos.getBattleGroupeByBarId(id);
+		List<BattleGroupe> myFuturEvents = new ArrayList<BattleGroupe>();
+		
+		for (BattleGroupe bg : myEvents) {
+			if (  ( bg.getDateEvent() ).after(d) ) {
+				myFuturEvents.add(bg);
+			};
+		};		
+		return myFuturEvents;
+	}
+
+	@RequestMapping(value = "/battlegroupes/old/{id}", method = RequestMethod.GET)
+	public List<BattleGroupe> MyOldEvents (@PathVariable Long id){
+		Date d = new Date();
+		List<BattleGroupe> myEvents = battleGroupRepos.getBattleGroupeByBarId(id);
+		List<BattleGroupe> myOldEvents = new ArrayList<BattleGroupe>();
+		
+		for (BattleGroupe bg : myEvents) {
+			if (  ( bg.getDateEvent() ).before(d) ) {
+				myOldEvents.add(bg);
+			};
+		};		
+		return myOldEvents;
+	}
+	
+
+	
 
 }
