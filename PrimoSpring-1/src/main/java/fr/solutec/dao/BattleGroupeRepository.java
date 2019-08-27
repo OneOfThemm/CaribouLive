a0b2019bc50934e6aceafae9353e4d0eda4d3532
CoralineReineOurs <coraline.leberre@gmail.com>
2019-08-27 10:47:48 +0200

    @query ORDER BY daetEvent DES

--- PrimoSpring-1/src/main/java/fr/solutec/dao/BattleGroupeRepository.java
+++ PrimoSpring-1/src/main/java/fr/solutec/dao/BattleGroupeRepository.java
@@ -1,13 +1,16 @@
 package fr.solutec.dao;
 
+
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 import fr.solutec.entities.BattleGroupe;
-import fr.solutec.entities.Event;
+
 
 public interface BattleGroupeRepository extends JpaRepository<BattleGroupe, Long>{
 	public List<BattleGroupe> findByNom (String nom);
@@ -15,11 +18,13 @@ public interface BattleGroupeRepository extends JpaRepository<BattleGroupe, Long
 	public List<BattleGroupe> findByDateEventBefore (Date date); 
 	public List<BattleGroupe> findByDateEventAfter (Date date);
 	
+	
+	@Query ("SELECT bg FROM BattleGroupe bg INNER JOIN bg.bar b  WHERE b.id = ?1 ORDER BY bg.dateEvent DESC")
 	public List<BattleGroupe> getBattleGroupeByBarId (@PathVariable Long id);
 
+	
+	
 	public List<BattleGroupe> getBattleGroupeByGenreNom (@PathVariable String nom);
-	
-	
-	// public int length ();
+
 	
 }
